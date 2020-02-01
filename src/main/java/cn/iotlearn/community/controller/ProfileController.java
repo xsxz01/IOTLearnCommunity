ffc16b5bf5857ca65017c0daae90d89aba9be79e
AaronDoge <qq1502724957@live.com>
2020-02-01 23:04:50 +0800

    添加profile部分路由逻辑 修改profile菜单样式

--- src/main/java/cn/iotlearn/community/controller/ProfileController.java
+++ src/main/java/cn/iotlearn/community/controller/ProfileController.java
@@ -1,14 +1,30 @@
 package cn.iotlearn.community.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ProfileController {
     @GetMapping("/profile/{action}")
-    public String profile(@PathVariable(name = "action",required = false) String action){
-
+    public String profile(@PathVariable(name = "action",required = false) String action,
+                          Model model
+                          ){
+        // 避免出现空指针
+        if ("questions".equals(action)){
+            model.addAttribute("section","questions");
+            model.addAttribute("sectionName","我的提问");
+        }else if ("replies".equals(action)){
+            model.addAttribute("section","replies");
+            model.addAttribute("sectionName","回复");
+        }else if ("settings".equals(action)){
+            model.addAttribute("section","settings");
+            model.addAttribute("sectionName","个人资料");
+        }else {
+            model.addAttribute("section","error");
+            model.addAttribute("sectionName","模块未找到");
+        }
         return "profile";
     }
 }
